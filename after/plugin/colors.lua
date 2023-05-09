8fe5193015fd2e06d108263c29175335c295bd30
Juan Sulca <juan.sulca@thoughtworks.com>
2023-05-09 09:53:25 -0500

    Initial setup

---
+++ after/plugin/colors.lua
@@ -0,0 +1,23 @@
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+	variant = 'auto',
+	--- @usage 'main'|'moon'|'dawn'
+	dark_variant = 'main',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = true,
+
+})
+
+function SetColors(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+SetColors("tokyonight")
+
