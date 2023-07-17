f34978a57a782d5e722d3a7ebc170e84c6f6e077
Juan Sulca <juan.sulca@thoughtworks.com>
2023-07-17 11:18:00 -0400

    Added treesitter context

--- lua/myconfig/packer.lua
+++ lua/myconfig/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
   use('folke/tokyonight.nvim')
 
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use("nvim-treesitter/nvim-treesitter-context")
 
 	use('tpope/vim-fugitive')
 
